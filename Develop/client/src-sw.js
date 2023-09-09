8e567d5d62cc51f0daa18a74e68102b6fa0089a8
Jake Norris <jacobnorris013@gmail.com>
2023-09-09 12:01:18 -0400

    init

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,30 +1,35 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
+const pageCache = new StaleWhileRevalidate({
   cacheName: 'page-cache',
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, 
     }),
   ],
 });
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
+registerRoute(({ request }) => request.mode === 'navigate', ({ event }) => {
+  return pageCache.handle({ event });
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => {
+
+  return /assets\//.test(request.url);
+}, new CacheFirst());
+
+
+offlineFallback({
+  pageFallback: '/index.html',
+  imageFallback: '/src/images/logo.png',
+});
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,26 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+let deferredPrompt;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  butInstall.style.display = 'block'; // Show the install button
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const choiceResult = await deferredPrompt.userChoice;
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the installation prompt');
+    } else {
+      console.log('User dismissed the installation prompt');
+    }
+    deferredPrompt = null;
+  }
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('App installed:', event);
+});
